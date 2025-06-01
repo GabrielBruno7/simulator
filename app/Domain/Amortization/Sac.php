3de6ce5628f1303f31576f656bf380b02ff3e4e4
gabrielbruno7 <gabriel.bruno1w@gmail.com>
2025-06-01 09:09:38 -0300

    Feat: Implement SAC methods

--- app/Domain/Amortization/Sac.php
+++ app/Domain/Amortization/Sac.php
@@ -2,12 +2,77 @@
 
 namespace App\Domain\Amortization;
 
+use App\Domain\Installment;
 use App\Domain\Loan;
 
 class Sac extends Amortization
 {
     public function calculateValues(Loan $loan): Loan
     {
+        $installments = $loan->getInstallments();
+        $pmt = $this->calculatePMT($loan);
+
+        $debitBalance = $loan->getValue();
+
+        foreach ($installments as $installment) {
+            $installment->setLoan($loan);
+
+            $this->calculateTotalValue($installment, $pmt);
+            $this->calculateInterestValue($installment, $debitBalance);
+            $this->calculateMainValue($installment);
+
+            $debitBalance = $this->setDebitBalance($installment, $debitBalance);
+        }
+
+        $loan->setInstallments($installments);
+
         return $loan;
+    }
+
+    public function calculateTotalValue(Installment $installment, float $pmt): void
+    {
+        $installment->setTotalValue($pmt);
+    }
+
+    public function calculateInterestValue(Installment $installment, float $debitBalance): void
+    {
+        $interestValue = round($debitBalance * $installment->getLoan()->getInterest(), 2);
+
+        $installment->setInterestValue($interestValue);
+    }
+
+    public function calculateMainValue(Installment $installment): void
+    {
+        $mainValue = round($installment->getLoan()->getValue() / $installment->getLoan()->getPeriod(), 2);
+
+        $installment->setMainValue($mainValue);
+    }
+
+    public function setDebitBalance(Installment $installment, float $previousDebitBalance): float
+    {
+        $newDebitBalance = round($previousDebitBalance - $installment->getMainValue(), 2);
+
+        if ($newDebitBalance < 0) {
+            $newDebitBalance = 0.0;
+        }
+
+        $installment->setDebitBalance($newDebitBalance);
+
+        return $newDebitBalance;
+    }
+
+    public function calculatePMT(Loan $loan): float
+    {
+        $pv = $loan->getValue();
+        $i = $loan->getInterest();
+        $n = $loan->getPeriod();
+
+        $pmt = $pv * (
+            $i * pow(1 + $i, $n)
+        ) / (
+            pow(1 + $i, $n) - 1
+        );
+
+        return round($pmt, 2);
     }
 }
